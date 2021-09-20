a71da6ede833d0e123c13d31a185f9a7a9207734
William Astorga <wastorga@altus.co.cr>
2021-09-20 09:13:07 -0600

    Update required providers syntax

--- examples/pnp_workflow/main.tf
+++ examples/pnp_workflow/main.tf
@@ -2,8 +2,9 @@
 terraform {
   required_providers {
     dnacenter = {
-      versions = ["0.0.3"]
+      version = "0.0.3"
       source   = "hashicorp.com/edu/dnacenter"
+      # "hashicorp.com/edu/dnacenter" is the local built source, change to "cisco-en-programmability/dnacenter" to use downloaded version from registry
     }
   }
 }
