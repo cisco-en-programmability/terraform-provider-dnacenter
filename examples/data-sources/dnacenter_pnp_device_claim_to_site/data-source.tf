1a1507cbfdd4461371b01eaa37bd10127d2ff2ed
William Astorga <wastorga@altus.co.cr>
2022-02-01 12:01:15 -0600

    Update formatting to examples

--- examples/data-sources/dnacenter_pnp_device_claim_to_site/data-source.tf
+++ examples/data-sources/dnacenter_pnp_device_claim_to_site/data-source.tf
@@ -5,15 +5,15 @@ data "dnacenter_pnp_device_claim_to_site" "example" {
   site_id   = "string"
   type      = "string"
   hostname  = "string"
-  config_info{
-    config_id= "string"
-    config_parameters{
-      key="string"
-      value= "string"
+  config_info {
+    config_id = "string"
+    config_parameters {
+      key   = "string"
+      value = "string"
     }
   }
-  image_info{
-    image_id= "string"
-    skip= "false"
+  image_info {
+    image_id = "string"
+    skip     = "false"
   }
 }
