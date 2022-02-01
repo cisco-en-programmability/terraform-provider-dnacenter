6d25129d5907134a770bcf0d9e2fd70e949ac2c1
fmunoz <fmunoz@altus.cr>
2022-02-01 11:59:11 -0600

    Update example for data source

--- examples/data-sources/dnacenter_pnp_device_claim_to_site/data-source.tf
+++ examples/data-sources/dnacenter_pnp_device_claim_to_site/data-source.tf
@@ -4,4 +4,16 @@ data "dnacenter_pnp_device_claim_to_site" "example" {
   device_id = "string"
   site_id   = "string"
   type      = "string"
+  hostname  = "string"
+  config_info{
+    config_id= "string"
+    config_parameters{
+      key="string"
+      value= "string"
+    }
+  }
+  image_info{
+    image_id= "string"
+    skip= "false"
+  }
 }
