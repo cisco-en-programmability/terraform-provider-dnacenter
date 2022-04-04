e5c1367ab54b7852dfff03393ce086f2b6dfa23f
fmunoz <fmunoz@altus.cr>
2022-04-04 15:13:03 -0600

    updating docs

--- examples/samples/resources/dnacenter_business_sda_wireless_controller/resource.tf
+++ examples/samples/resources/dnacenter_business_sda_wireless_controller/resource.tf
@@ -16,8 +16,8 @@ provider "dnacenter" {
 resource "dnacenter_business_sda_wireless_controller" "example" {
   provider = dnacenter
   parameters {
-    device_name           = "C9K-Branch-SFO.dcloud.cisco.com"
-    site_name_hierarchy   = "Global/San Francisco"
+    device_name         = "C9K-Branch-SFO.dcloud.cisco.com"
+    site_name_hierarchy = "Global/San Francisco"
   }
 }
 
@@ -26,7 +26,7 @@ output "dnacenter_business_sda_wireless_controller_example" {
 }
 
 data "dnacenter_dnacaap_management_execution_status" "example" {
-  depends_on = [dnacenter_business_sda_wireless_controller.example]
+  depends_on   = [dnacenter_business_sda_wireless_controller.example]
   provider     = dnacenter
   execution_id = dnacenter_business_sda_wireless_controller.example.item.0.execution_id
 }
--- examples/samples/resources/dnacenter_wireless_provision_access_point/resource.tf
+++ examples/samples/resources/dnacenter_wireless_provision_access_point/resource.tf
@@ -14,14 +14,14 @@ resource "dnacenter_wireless_provision_access_point" "example" {
     create_before_destroy = true
   }
   parameters {
-    payload{
-    custom_ap_group_name   = "string"
-    custom_flex_group_name = ["string"]
-    device_name            = "string"
-    rf_profile             = "string"
-    site_id                = "string"
-    site_name_hierarchy    = "string"
-    type                   = "string"
+    payload {
+      custom_ap_group_name   = "string"
+      custom_flex_group_name = ["string"]
+      device_name            = "string"
+      rf_profile             = "string"
+      site_id                = "string"
+      site_name_hierarchy    = "string"
+      type                   = "string"
     }
   }
 }
