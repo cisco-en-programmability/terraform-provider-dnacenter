7c9e6535680e9bb6007fc0d1667c0999b6257dc3
fmunoz <fmunoz@altus.cr>
2022-07-10 23:17:14 -0600

    ## 1.0.2-beta (July 10, 2022) BUGFIXES: * `resource_sda_virtual_network` is now able to create the resource. * `resource_site` is now able to read resource information.
    IMPROVEMENTS:
    * An example for resource `dnacenter_sda_virtual_network` was added.

---
+++ examples/samples/resources/dnacenter_sda_virtual_network/resource.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    dnacenter = {
+      version = "1.0.1-beta"
+      source  = "hashicorp.com/edu/dnacenter"
+      # "hashicorp.com/edu/dnacenter" is the local built source, change to "cisco-en-programmability/dnacenter" to use downloaded version from registry
+    }
+  }
+}
+
+resource "dnacenter_sda_virtual_network" "example" {
+  provider = dnacenter
+  parameters {
+    payload{
+      site_name_hierarchy  = "Global/New Jersey/MurrayHill/test/TestFloor"
+      virtual_network_name = "TEST_VNs"
+    }
+  }
+}
+
+output "dnacenter_sda_virtual_network_example" {
+  value = dnacenter_sda_virtual_network.example
+}
