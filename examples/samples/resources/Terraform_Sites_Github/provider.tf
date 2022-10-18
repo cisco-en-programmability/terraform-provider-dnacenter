dd44c7e7549d00ec2fb0fb039385fcf6ac0e8b9a
fmunoz <fmunoz@altus.cr>
2022-10-18 11:19:44 -0600

    new version 1.0.9-beta

---
+++ examples/samples/resources/Terraform_Sites_Github/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    dnacenter = {
+      source  = "hashicorp.com/edu/dnacenter"
+      version = "1.0.9-beta"
+    }
+  }
+}
+
+# provider "dnacenter" {
+#   username   = "***"
+#   password   = "***"
+#   base_url   = "https://U.V.W.X"
+#   debug      = "true"
+#   ssl_verify = "false"
+# }
