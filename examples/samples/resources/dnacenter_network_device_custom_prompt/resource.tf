2fbcf2929d99584bca4697bc591a1737caf1996b
fmunoz <fmunoz@altus.cr>
2023-01-10 09:12:56 -0600

    new version 1.0.15-beta

--- examples/samples/resources/dnacenter_network_device_custom_prompt/resource.tf
+++ examples/samples/resources/dnacenter_network_device_custom_prompt/resource.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     dnacenter = {
-      version = "1.0.14-beta"
+      version = "1.0.15-beta"
       source  = "hashicorp.com/edu/dnacenter"
       # "hashicorp.com/edu/dnacenter" is the local built source change to "cisco-en-programmability/dnacenter" to use downloaded version from registry
     }
