84165f6e30e8c56a7c75cce89e4d0063981a4504
fmunoz <fmunoz@altus.cr>
2023-04-27 11:30:25 -0600

    New version v1.1.1-beta

--- examples/samples/resources/dnacenter_app_policy_intent_create/resource.tf
+++ examples/samples/resources/dnacenter_app_policy_intent_create/resource.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     dnacenter = {
-      version = "1.1.0-beta"
+      version = "1.1.1-beta"
       source  = "hashicorp.com/edu/dnacenter"
       # hashicorp.com/edu/dnacenter is the local built source change to cisco-en-programmability/dnacenter to use downloaded version from registry
     }
