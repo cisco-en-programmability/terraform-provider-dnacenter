74aeab149cbff0a08e1849148ffda841d8fd105c
Arrokoth-byte <dbesquivel12@gmail.com>
2023-09-28 11:36:30 -0600

    Fixed create user ( issue #213 )

---
+++ examples/samples/resources/dnacenter_user/resource.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    dnacenter = {
+      version = "1.1.18-beta"
+      source  = "hashicorp.com/edu/dnacenter"
+      # "hashicorp.com/edu/dnacenter" is the local built source change to "cisco-en-programmability/dnacenter" to use downloaded version from registry
+    }
+  }
+}
+
+provider "dnacenter" {
+  debug = "true"
+}
+
+resource "dnacenter_user" "example" {
+  provider = dnacenter
+  parameters {
+
+    email      = "john.doe@example.com"
+    first_name = "John"
+    last_name  = "Doe"
+    password   = "Secure_password"
+    role_list  = ["63060e76f5bc0031ecc68c14"]
+    user_id    = "johndoe123"
+    username   = "johndoe"
+  }
+}
+
+output "dnacenter_user_example" {
+  sensitive = true
+  value = dnacenter_user.example
+}
