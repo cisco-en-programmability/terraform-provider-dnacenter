d2bb755340c9f2fd4a5df8ff10b4f98d6337e5ed
fmunoz <fmunoz@altus.cr>
2023-11-20 13:37:48 -0600

    Resource dnacenter_reserve_ip_subpool does not detect changes made outside terraform #217

--- examples/samples/resources/dnacenter_wireless_profile/resource.tf
+++ examples/samples/resources/dnacenter_wireless_profile/resource.tf
@@ -34,16 +34,6 @@ resource "dnacenter_wireless_profile" "example" {
         name           = "BTest22"
         type           = "string"
       }
-       ssid_details {
-        enable_fabric = "true"
-        flex_connect {
-          enable_flex_connect = "false"
-          local_to_vlan       = 0
-        }
-        interface_name = "management"
-        name           = "new2"
-        type           = "eduroam"
-      }
       ssid_details {
         enable_fabric = "true"
         flex_connect {
@@ -54,7 +44,7 @@ resource "dnacenter_wireless_profile" "example" {
         name           = "ATest222"
         type           = "eduroam"
       }
-      ssid_details {
+       ssid_details {
         enable_fabric = "true"
         flex_connect {
           enable_flex_connect = "false"
@@ -62,9 +52,8 @@ resource "dnacenter_wireless_profile" "example" {
         }
         interface_name = "management"
         name           = "new"
-        type           = "eduroam"
+        type           = "string"
       }
-      
     }
   }
 }
